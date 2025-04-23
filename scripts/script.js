cca802fdb457dbda485d369e715b5a2b5ee9aa64
Irina-Gorges <r3nategr4nate@gmail.com>
2025-04-23 22:02:42 +0200

    added food container

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
 function init() {
-    renderMeals();
+  renderMeals();
 }
 
 function renderMeals() {
-    const contentRef = document.getElementById("food_container");
-    contentRef.innerHTML = "";
-    for (let i = 0; i < meals.length; i++) {
-        contentRef.innerHTML += getMealsTemplate(i);
-    }
+  const contentRef = document.getElementById("food_container");
+  contentRef.innerHTML = "";
+  for (let i = 0; i < meals.length; i++) {
+    contentRef.innerHTML += getMealsTemplate(i);
+  }
 }
+
+// Optional: Gerichte Slider hinzufügen
+// Funktion zum Essen zum Warenkorb hinzufügen
+// Funktion zum Essen aus dem Warenkorb entfernen
+// Funktion zum Essen im Warenkorb anzeigen
+// Funktion zum Essen im Warenkorb leeren
+// Funktion zum Essen im Warenkorb preis berechnen
+// Funktion zum Essen im Warenkorb preis anzeigen
+// Optional: Lieferkosten berechnen
--- scripts/template.js
+++ scripts/template.js
@@ -1,6 +1,12 @@
 function getMealsTemplate(i) {
-    return `<div id="mealscontent${i}" class="mealscontainer">
-            <img class="food_photo" src="img/${meals[i].image}" alt=""> 
-            <p id="name_meal">${meals[i].name}</p><p id="price_meal">${meals[i].price}</p><span id="description_meal">${meals[i].description}</span>
-        </div>`;
+  return `<div id="mealscontent${i}" class="mealscontainer">
+            <img class="food_photo" src="img/${
+              meals[i].image
+            }" alt=""><div id="info_meal"><p id="name_meal" class="namemeal">${
+    meals[i].name
+  }</p><p id="price_meal">${meals[i].price.toFixed(
+    2
+  )}&nbsp;€</p><span id="description_meal">${meals[i].description}</span></div>
+          
+        <button id="btn${i}" class="btn">+</button></div>`;
 }
