696f75b231ef49c3a4bc6667a28ba13d55147767
Irina-Gorges <r3nategr4nate@gmail.com>
2025-04-25 18:00:11 +0200

    added remove function

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,50 @@
 function init() {
-  renderMeals();
+    renderMeals();
 }
 
 function renderMeals() {
-  const contentRef = document.getElementById("food_container");
-  contentRef.innerHTML = "";
-  for (let i = 0; i < meals.length; i++) {
-    contentRef.innerHTML += getMealsTemplate(i);
-  }
+    const contentRef = document.getElementById("food_Container");
+    contentRef.innerHTML = "";
+    for (let i = 0; i < meals.length; i++) {
+        contentRef.innerHTML += getMealsTemplate(i);
+    }
 }
 
 function addToCard(i) {
-  let cardContRef = document.getElementById("basket");
-  if (document.getElementById(`${meals[i].name}`)) {
-  } else {
-    cardContRef.innerHTML += addToCardTemplate(i);
-  }
+    let cardContRef = document.getElementById("basket");
+    if (document.getElementById(`${meals[i].name}`)) {
+    } else {
+        cardContRef.innerHTML += addToCardTemplate(i);
+    }
 }
 
+function removeFromCard(i) {
+    let deleteRef = document.getElementById(`${meals[i].name}`);
+    if (deleteRef) {
+        deleteRef.remove();
+    }
+}
 
+function calculatePrice(w, i) {
+    switch (w) {
+        case "-":
+            document.getElementById(`${meals[i].price}`) - 1;
+            break;
+
+        case "+":
+            document.getElementById(`${meals[i].price}`) + 1;
+            break;
+
+        default:
+            document.getElementById(`${meals[i].price}`);
+            break;
+    }
+}
 // Optional: Gerichte Slider hinzufügen
-// Funktion zum Essen zum Warenkorb hinzufügen
+
 // Funktion zum Essen aus dem Warenkorb entfernen
-// Funktion zum Essen im Warenkorb anzeigen
+
 // Funktion zum Essen im Warenkorb leeren
 // Funktion zum Essen im Warenkorb preis berechnen
 // Funktion zum Essen im Warenkorb preis anzeigen
 // Optional: Lieferkosten berechnen
- 
--- scripts/template.js
+++ scripts/template.js
@@ -1,22 +1,23 @@
 function getMealsTemplate(i) {
-  return `<div id="mealsContent${i}" class="mealsContainer">
+    return `<div id="mealsContent${i}" class="mealsContainer">
   <img class="food_Photo" src="img/${meals[i].image}" alt="">
             <div id="info_Meal" class="infoMeal"><p id="name_Meal" class="nameMeal">${
-              meals[i].name
+                meals[i].name
             }</p><p id="price_Meal" class="priceMeal">${meals[i].price.toFixed(
-    2
-  )}&nbsp;€</p><p id="description_Meal" class="descriptionMeal">${
-    meals[i].description
-  }</p></div><button id="btn${i}" class="btn" onclick="addToCard(${i})">+</button></div>`;
+        2
+    )}&nbsp;€</p><p id="description_Meal" class="descriptionMeal">${
+        meals[i].description
+    }</p></div><button id="btn${i}" class="btn" onclick="addToCard(${i})">+</button></div>`;
 }
 
 function addToCardTemplate(i) {
-  return `<div id="${
-    meals[i].name
-  }" class="info_MealW"><p id="name_Meal" class="nameMealW">${
-    meals[i].name
-  }</p><div id="calc"><button class="operator" onclick="calculatePrice("-")">-</button><div id="multiplier">1</div><button class="operator" onclick="calculatePrice("+")>+</button><p id="price_Meal" class="priceMealW">${meals[
-    i].price.toFixed(
-    2
-  )}&nbsp;€</p><button id="clear" onclick="removeFromCard(${i})">&#128465;</button></div></div>`;
+    return `<div id="${
+        meals[i].name
+    }" class="info_MealW"><p id="name_Meal" class="nameMealW">${
+        meals[i].name
+    }</p><div id="calc"><button class="operator" onclick="calculatePrice("-")">-</button><div id="multiplier">1</div><button class="operator" onclick="calculatePrice("+")>+</button><p id="price_Meal" class="priceMealW">${meals[
+        i
+    ].price.toFixed(
+        2
+    )}&nbsp;€</p><button id="clear" onclick="removeFromCard(${i})">&#128465;</button></div></div>`;
 }
